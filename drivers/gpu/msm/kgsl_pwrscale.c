5524bd4e95020b4c082bd346f8d02bd78c9fc6e3
Tarun Karra <tkarra@codeaurora.org>
2014-03-27 12:58:45 -0700

    msm: kgsl: Add new API's to track device mutex owner
    Add new API's kgsl_mutex_lock/unlock to track device mutex owner.
    Mutex owner is an atomic variable and all operations set/clear on
    it are also atomic to avoid any race conditions.
    
    If a thread tries to acquire the mutex using new API, try to acquire
    the mutex only if mutex owner is not same as current thread. This is
    to prevent deadlocks which can happen if a thread tries to acquire
    mutex with mutex held.
    
    Change-Id: I3da4d0427ede5458f6a663043c57a4799b72d8fd
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -195,7 +195,7 @@ int kgsl_devfreq_target(struct device *dev, unsigned long *freq, u32 flags)
 
 	pwr = &device->pwrctrl;
 
-	mutex_lock(&device->mutex);
+	kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 	cur_freq = kgsl_pwrctrl_active_freq(pwr);
 	level = pwr->active_pwrlevel;
 
@@ -232,7 +232,7 @@ int kgsl_devfreq_target(struct device *dev, unsigned long *freq, u32 flags)
 
 	*freq = kgsl_pwrctrl_active_freq(pwr);
 
-	mutex_unlock(&device->mutex);
+	kgsl_mutex_unlock(&device->mutex, &device->mutex_owner);
 	return 0;
 }
 EXPORT_SYMBOL(kgsl_devfreq_target);
@@ -259,7 +259,7 @@ int kgsl_devfreq_get_dev_status(struct device *dev,
 
 	pwrscale = &device->pwrscale;
 
-	mutex_lock(&device->mutex);
+	kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 	/* make sure we don't turn on clocks just to read stats */
 	if (device->state == KGSL_STATE_ACTIVE) {
 		struct kgsl_power_stats extra;
@@ -287,7 +287,7 @@ int kgsl_devfreq_get_dev_status(struct device *dev,
 	trace_kgsl_pwrstats(device, stat->total_time, &pwrscale->accum_stats);
 	memset(&pwrscale->accum_stats, 0, sizeof(pwrscale->accum_stats));
 
-	mutex_unlock(&device->mutex);
+	kgsl_mutex_unlock(&device->mutex, &device->mutex_owner);
 
 	return 0;
 }
@@ -310,9 +310,9 @@ int kgsl_devfreq_get_cur_freq(struct device *dev, unsigned long *freq)
 	if (freq == NULL)
 		return -EINVAL;
 
-	mutex_lock(&device->mutex);
+	kgsl_mutex_lock(&device->mutex, &device->mutex_owner);
 	*freq = kgsl_pwrctrl_active_freq(&device->pwrctrl);
-	mutex_unlock(&device->mutex);
+	kgsl_mutex_unlock(&device->mutex, &device->mutex_owner);
 
 	return 0;
 }
