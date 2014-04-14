ff0896b4a6629e3d4288df770a5eb31356c65b07
Luka Karinja <lk@varjanta.com>
2014-04-14 17:46:16 +0200

    Revert "mm: add a low limit to alloc_large_system_hash"
    This reverts commit a343ad8a53d7444d995e6a5772db0079508ec784.

--- fs/dcache.c
+++ fs/dcache.c
@@ -3066,7 +3066,6 @@ static void __init dcache_init_early(void)
 					HASH_EARLY,
 					&d_hash_shift,
 					&d_hash_mask,
-					0,
 					0);
 
 	for (loop = 0; loop < (1U << d_hash_shift); loop++)
@@ -3097,7 +3096,6 @@ static void __init dcache_init(void)
 					0,
 					&d_hash_shift,
 					&d_hash_mask,
-					0,
 					0);
 
 	for (loop = 0; loop < (1U << d_hash_shift); loop++)
